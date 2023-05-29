c3f5c7d92d247e7132046ef5691f67c6de94b4ec
NehaDasar <fadewar.neha@gmail.com>
2023-05-29 23:40:40 +0530

    signin component

---
+++ angularproj1/src/app/signin/signin.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent {
+  formTitle = '--LOGIN FORM--';
+  test!: string;
+
+submit() {
+  let num = 20;
+  num = 2000;
+  //this.test = 'testing property';
+  this.formTitle = 'Sign U Form';
+}
+}
