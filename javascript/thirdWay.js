088a6756af2a4b0dcbcbe2b3f6b2c5db612ce132
NehaDasar <fadewar.neha@gmail.com>
2023-05-30 00:21:51 +0530

    secthird way js

---
+++ javascript/thirdWay.js
@@ -0,0 +1,25 @@
+
+// syntax: function functionName(parameter 1,para2,....){
+    //code block
+//}
+
+function test(){
+    test = 'Neha';
+    xyz = 1996;
+   document.write(test);
+   console.log(test);
+}
+
+let firstName = 'Neha Dasarwar';
+console.log(firstName);
+
+function testParametrisedFun(a,b){
+ let res = a - b;
+ console.log(res);
+}
+
+function parameterLessFun(){
+   let result = 100 + 90 ;
+   console.log(result);
+}
+
